e6ef3767c0a041c07ed27be55ccdab0376bb99b8
João Paulo Marquesini <jmarquesini@gmail.com>
2020-09-24 10:42:15 -0300

    v1.7.34

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
-flutter clean
-flutter build appbundle --release
-flutter build ios --release --no-codesign
-cd android
-fastlane deploy
-cd ..
+# flutter clean
+# flutter build appbundle --release
+# flutter build ios --release --no-codesign
+# cd android
+# fastlane deploy
+# cd ..
 cd ios
 fastlane release
